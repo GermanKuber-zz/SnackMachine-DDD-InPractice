01f9e9d86b2afbb93bb97bd7d28e651e3894bdff
Germán Küber <german.kuber@outlook.com>
2018-05-16 23:00:39 -0300

    Added new First version

---
+++ SnackMachine/SnackMachine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SnackMachine.Core
+{
+    public sealed class SnackMachine : Entity
+    {
+        public Money InternalMoney { get; private set; } = Money.None;
+        public Money TransactionMoney { get; private set; } = Money.None;
+
+        public void InsertMoney(Money money)
+        {
+            if (money == Money.None)
+                throw  new InvalidOperationException();
+            TransactionMoney += money;
+        }
+
+
+        public Money ReturnMoney(Money money) =>
+             TransactionMoney = Money.None;
+
+        public void Buy()
+        {
+            InternalMoney += TransactionMoney;
+            TransactionMoney = Money.None;
+        }
+    }
+}
